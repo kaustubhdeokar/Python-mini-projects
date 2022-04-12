b6cac0918259a12e1ba444da9f3b42807e791df4
kaustubhdeokar <kaustubhd9@gmail.com>
2022-04-12 08:42:14 +0530

    user addition & quering added-routing messed up

--- app.py
+++ app.py
@@ -17,6 +17,39 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
+class Users(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.userid
+
+
+@app.route("/adduser", methods=['POST', 'GET'])
+def adduser():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    new_user = Users(username=username, password=password)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        return 'adding user failed'
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    try:
+        user = Users.query.filter_by(username=username).first()
+        return f' {user.username} ' + " " + str(user.password)
+    except Exception as e:
+        return 'failure'
+
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -33,7 +66,7 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template("index.html", tasks=tasks)
+        return render_template("login.html", tasks=tasks)
 
 
 @app.route("/delete/<int:id>")
